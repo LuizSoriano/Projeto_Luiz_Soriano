9865a7b6561825b3944fc340828ec67ffc065e37
LuizSoriano <luizvictor0205@gmail.com>
2023-09-27 18:22:03 -0300

    Criado o Metodo MA3()

--- A.java
+++ A.java
@@ -34,4 +34,8 @@ public class A {
     public void MA2(){
         System.out.println("MA2");
     }
+
+    public void MA3(){
+        System.out.println("Alteração da classe A do clone");
+    }
 }
