66242f54a126001a3c91c24a47399211e7cffbae
LuizSoriano <luizvictor0205@gmail.com>
2023-09-27 18:18:38 -0300

    Criacao da Classe C

---
+++ C.java
@@ -0,0 +1,18 @@
+public class C {
+    private String C1;
+    private String C2;
+
+    public C(){
+        C1 = "Luiz";
+        C2 = "Selene";
+    }
+
+    public void MC1(){
+        System.out.println("MC1");
+    }
+
+    public void MC2(){
+        System.out.println(("MC2"));
+    }
+    
+}
